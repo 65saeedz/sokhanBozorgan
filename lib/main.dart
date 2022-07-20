4da8812cd9aabaf2980e24ea7fcbd00c2d0cd982
65saeedz <76889604+65saeedz@users.noreply.github.com>
2022-07-20 15:37:10 +0430

    second edition

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-    Repository repository=Repository();
+  Repository repository = Repository();
   late Future<Sohkanmodel> sokhan;
-  @override
-  void initState() {
-    sokhan = repository.getNewSokhan();
-    super.initState();
-  }
+  int times = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -38,33 +34,57 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Container(
-            child: Column(
-              children: [
-                FutureBuilder<Sohkanmodel>(
-                  future: sokhan,
-                  builder: ((context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Text(
-                       snapshot.data!.content!,
-                        style: TextStyle(color: Colors.black),
-                      );
-                    } else {
-                      return Center(child: CircularProgressIndicator());
-                    }
-                  }),
-                ),
-              ],
-            ),
+            height: 400,
+            width: 300,
+            child: times == 0
+                ? Center(child: Text('Tap the button'))
+                : Container(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                    child: FutureBuilder<Sohkanmodel>(
+                      future: repository.getNewSokhan(),
+                      builder: ((context, snapshot) {
+                        switch (snapshot.connectionState) {
+                          case ConnectionState.waiting:
+                            return Center(child: CircularProgressIndicator());
+                          case ConnectionState.done:
+                          default:
+                            if (snapshot.hasData) {
+                              return Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    snapshot.data!.content!,
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text('( ${snapshot.data!.author!} )')
+                                ],
+                              );
+                            } else {
+                              return Center(child: CircularProgressIndicator());
+                            }
+                        }
+                      }),
+                    ),
+                  ),
             decoration: BoxDecoration(
               color: Colors.grey,
             ),
-            constraints: BoxConstraints(
-                maxHeight: 400, minWidth: 300, maxWidth: 300, minHeight: 200),
+            // constraints: BoxConstraints(
+            //   maxHeight: 400,
+            //   maxWidth: 300,
+            //   minHeight: 200,
+            //   minWidth: 300,
+            // ),
           ),
           ElevatedButton(
               onPressed: () {
                 setState(() {
-                sokhan=  repository.getNewSokhan();
+                  times++;
+                  sokhan = repository.getNewSokhan();
                 });
               },
               child: Text('Give me a Qout')),
